090bed97ae39e3ec18331f289344475902acfe08
Alex Belyaev <t433621@yandex.ru>
2022-12-04 12:57:43 +0300

    1000

---
+++ easy1000.java
@@ -0,0 +1,21 @@
+public class easy1000 {
+    /*
+     * Вывести все простые числа от 1 до 1000.
+     */
+
+    public static void main(String[] args) {
+        boolean flag = true;
+        for (int i = 1; i <= 1000; i++) {
+            flag = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                System.out.println(i);
+            }
+        }
+    }
+}
